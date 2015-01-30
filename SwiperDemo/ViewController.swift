f00b6de2201233abdd9e5d06e44e581154ae4b5a
Ruslan Alikhamov <r.alikhamov@gmail.com>
2015-01-30 21:44:34 +0100

    Initial Commit

---
+++ SwiperDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiperDemo
+//
+//  Created by Ruslan Alikhamov on 30/01/15.
+//  Copyright (c) 2015 Ruslan Alikhamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
